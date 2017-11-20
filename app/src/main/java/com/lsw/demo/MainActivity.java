45e6ab0ce0831decf88dad03876eef402f2eeee2
liushuwei <liushuwei@qiyi.com>
2017-11-20 16:15:36 +0800

    增加动画的多种用法

--- app/src/main/java/com/lsw/demo/MainActivity.java
+++ app/src/main/java/com/lsw/demo/MainActivity.java
@@ -2,6 +2,8 @@ package com.lsw.demo;
 
 import android.animation.Animator;
 import android.animation.AnimatorInflater;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.os.Bundle;
@@ -10,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewTreeObserver;
+import android.view.animation.BounceInterpolator;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -54,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
 //        setColorViewAnimation();
 
         // 创建包装类,并传入动画作用的对象
-        wrapper = new ViewWrapper(mButton);
+        /*wrapper = new ViewWrapper(mButton);
 
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,7 +65,13 @@ public class MainActivity extends AppCompatActivity {
                 ObjectAnimator.ofInt(wrapper, "width", 500).setDuration(3000).start();
                 // 设置动画的对象是包装类的对象
             }
-        });
+        });*/
+
+//        setMultiAnimator();
+
+//        setMultiXmlAnimator();
+
+        setViewPropertyAnimator();
 
     }
 
@@ -150,6 +159,65 @@ public class MainActivity extends AppCompatActivity {
         animator.setDuration(5000);
         animator.start();
 
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                //动画开始时执行
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                //动画重复时执行
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                //动画取消时执行
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                //动画结束时执行
+            }
+        });
+
+
+        animator.addListener(new AnimatorListenerAdapter() {
+            // 向addListener()方法中传入适配器对象AnimatorListenerAdapter()
+            // 由于AnimatorListenerAdapter中已经实现好每个接口
+            // 所以这里不实现全部方法也不会报错
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                super.onAnimationCancel(animation);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                super.onAnimationRepeat(animation);
+            }
+
+            @Override
+            public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+                // 如想只想监听动画开始时刻，就只需要单独重写该方法就可以
+            }
+
+            @Override
+            public void onAnimationPause(Animator animation) {
+                super.onAnimationPause(animation);
+            }
+
+            @Override
+            public void onAnimationResume(Animator animation) {
+                super.onAnimationResume(animation);
+            }
+        });
+
     }
 
 
@@ -159,6 +227,48 @@ public class MainActivity extends AppCompatActivity {
         animator.start();
     }
 
+
+    private void setMultiAnimator(){
+
+        float curTranslationX = mButton.getTranslationX();
+        // 步骤1：设置需要组合的动画效果
+        ObjectAnimator translation = ObjectAnimator.ofFloat(mButton, "translationX", curTranslationX, 300,curTranslationX);
+        // 平移动画
+        ObjectAnimator rotate = ObjectAnimator.ofFloat(mButton, "rotation", 0f, 360f);
+        // 旋转动画
+        ObjectAnimator alpha = ObjectAnimator.ofFloat(mButton, "alpha", 1f, 0f, 1f);
+        // 透明度动画
+
+        // 步骤2：创建组合动画的对象
+        AnimatorSet animSet = new AnimatorSet();
+
+        // 步骤3：根据需求组合动画
+        animSet.play(translation).with(rotate).before(alpha);
+        animSet.setDuration(5000);
+
+        // 步骤4：启动动画
+        animSet.start();
+
+    }
+
+    private void setMultiXmlAnimator(){
+        AnimatorSet animator = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.multi_set_animation);
+        // 创建组合动画对象  &  加载XML动画
+        animator.setTarget(mButton);
+        // 设置动画作用对象
+        animator.start();
+        // 启动动画
+    }
+
+
+    private void setViewPropertyAnimator(){
+        mButton.animate().alpha(0f);
+        // 单个动画设置:将按钮变成透明状态
+        mButton.animate().alpha(0f).setDuration(5000).setInterpolator(new BounceInterpolator());
+        // 单个动画效果设置 & 参数设置
+        mButton.animate().alpha(0f).x(500).y(500).alpha(1f);
+        // 组合动画:将按钮变成透明状态再移动到(500,500)处
+    }
 
     // 提供ViewWrapper类,用于包装View对象
     // 本例:包装Button对象
